52a542385b427fc5c810f8accba87429e1ad4b02
yogiduzit <yverma2@my.bcit.ca>
2021-03-06 15:03:01 -0800

    Functionality complete

--- Functions/HttpWebAPI.cs
+++ Functions/HttpWebAPI.cs
@@ -66,5 +66,55 @@ namespace Snoopy.Function
 
             return new CreatedResult(new Uri($"/students/{student.StudentId}", UriKind.Relative), student);
         }
+
+        [FunctionName("UpdateStudent")]
+        public async Task<IActionResult> UpdateStudent([HttpTrigger(AuthorizationLevel.Anonymous, "put", Route = "students/{id}")] HttpRequest req, 
+        string id, ILogger log)
+        {
+            log.LogInformation("C# HTTP GET/posts trigger function processed a request.");
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            Student student = JsonConvert.DeserializeObject<Student>(requestBody);
+            
+            if (id != student.StudentId) {
+                return new BadRequestObjectResult("StudentId in request must match StudentId in route, SMH");
+            }
+
+            _context.Entry(student).State = EntityState.Modified;
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                bool exists = await _context.Students.AnyAsync(s => s.StudentId == id);
+                if (!exists)
+                {
+                    return new NotFoundObjectResult(new { id });
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return new NoContentResult();
+        }
+
+        [FunctionName("DeleteStudent")]
+        public async Task<IActionResult> DeleteStudent([HttpTrigger(AuthorizationLevel.Anonymous, "delete", Route = "students/{id}")] HttpRequest req, 
+        string id, ILogger log)
+        {
+            log.LogInformation("C# HTTP GET/posts trigger function processed a request.");
+
+            Student student = await _context.Students.FindAsync(id);
+            if (student == null) {
+                return new NotFoundObjectResult(new { id });
+            }
+
+            _context.Students.Remove(student);
+            await _context.SaveChangesAsync();
+
+            return new NoContentResult();
+        }
     }
 }
